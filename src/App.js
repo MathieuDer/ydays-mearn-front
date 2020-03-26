676baf2ce74220ff96db87e1c74f09b12e88297e
mathieu derouiche <mathieu.derouiche@ynov.com>
2020-03-26 10:16:24 +0100

    Add auth, image upload, connection with NodeJS API, add Nav, add Sidebar, add validators, add Modal, add Places

--- src/App.js
+++ src/App.js
@@ -1,74 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-import Header from './components/Header/Header';
-import NewProduct from './components/Products/NewProduct';
-import ProductList from './components/Products/ProductList';
-import './App.css';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
 
-function App() {
-  const [loadedProducts, setLoadedProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+const App = () => {
+  const { token, login, logout, userId } = useAuth();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:5000/products');
+  let routes;
 
-      const responseData = await response.json();
-
-      setLoadedProducts(responseData.products);
-      setIsLoading(false);
-    };
-
-    fetchProducts();
-  }, []);
-
-  const addProductHandler = async (productName, productPrice) => {
-    try {
-      const newProduct = {
-        title: productName,
-        price: +productPrice // "+" to convert string to number
-      };
-      let hasError = false;
-      const response = await fetch('http://localhost:5000/product', {
-        method: 'POST',
-        body: JSON.stringify(newProduct),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-
-      if (!response.ok) {
-        hasError = true;
-      }
-
-      const responseData = await response.json();
-
-      if (hasError) {
-        throw new Error(responseData.message);
-      }
-
-      setLoadedProducts(prevProducts => {
-        return prevProducts.concat({
-          ...newProduct,
-          id: responseData.product.id
-        });
-      });
-    } catch (error) {
-      alert(error.message || 'Something went wrong!');
-    }
-  };
+  if (token) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
 
   return (
-    <React.Fragment>
-      <Header />
-      <main>
-        <NewProduct onAddProduct={addProductHandler} />
-        {isLoading && <p className="loader">Loading...</p>}
-        {!isLoading && <ProductList items={loadedProducts} />}
-      </main>
-    </React.Fragment>
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        login: login,
+        logout: logout
+      }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
   );
-}
+};
 
 export default App;
